25434c1f3e596845e181643bed7d40acf3d0fb84
yangchengwei <yangchengwei@senseauto.com>
2023-08-21 21:28:58 +0800

    use argparse to format cli app

--- day2/fashion_mnist_demo.py
+++ day2/fashion_mnist_demo.py
@@ -2,9 +2,10 @@ from mxnet import gluon
 from mxnet import autograd
 from mxnet import init
 import  matplotlib.pyplot as plt 
-gdata = gluon.data
+from mxnet.gluon import data as gdata
 
 
+#采用 gluon 的方式来训练衣物分类模型的例子
 mnist_train = gdata.vision.FashionMNIST(train=True)
 mnist_test = gdata.vision.FashionMNIST(train=False)
 
@@ -31,27 +32,23 @@ def get_fashion_mnist_labels(labels):  #@save
 
 # X, y = mnist_train[:18]
 # show_images(X.squeeze(axis=-1), 2, 9, titles=get_fashion_mnist_labels(y))
-batch_size=  100
+batch_size=  200
 totensor = gdata.vision.transforms.ToTensor()
 train_iter = gdata.DataLoader(dataset=mnist_train.transform_first(totensor), batch_size=batch_size, shuffle=False, num_workers=1)
 test_iter = gdata.DataLoader(dataset=mnist_test.transform_first(totensor), batch_size=batch_size, shuffle=True, num_workers=1)
 
-for X, y in train_iter:
-    print(f"X shape: {X.shape}")
-    print(f"y shape: {y.shape}")
-    break
 
-# 这个数据集的类别数是多少，因为这个和输出层的值要相等的，比如有 120 个类别，输出层的节点数应该也是120
+
+# 有 10 个类别，输出层的节点数应该也是10
 net =gluon.nn.Sequential()
-net.add(gluon.nn.Dense(256, activation='relu'))
-net.add(gluon.nn.Dense(10))
+net.add(gluon.nn.Dense(256, activation='relu'), gluon.nn.Dense(10))
 
 net.initialize(init.Normal(sigma=0.01))
 
 loss = gluon.loss.SoftmaxCrossEntropyLoss()
 
 
-trainer = gluon.Trainer(net.collect_params(), "sgd", {'learning_rate': 0.03})
+trainer = gluon.Trainer(net.collect_params(), "sgd", {'learning_rate': 0.5})
 import numpy as np 
     
 def cal_accuracy(data_iter):
@@ -61,19 +58,39 @@ def cal_accuracy(data_iter):
         pred = np.argmax(ret , axis=1)
         pt += (pred.astype(y.dtype) == y).sum().asscalar()
         total += y.shape[0]
-    print(f"pt:{type(pt)}, total:{type(total)}")
+    # print(f"pt:{type(pt)}, total:{type(total)}")
     return float(pt/total)
 
 def training(epoch = 5):
     for epoch in range(epoch):
+        total_loss = 0.0
+        iter_count = 0
         for X, y in train_iter:
             with autograd.record():
                 l = loss(net(X), y)
+                # print("type of ls is ", type(l), "shape of l is ", l.shape)
+                total_loss += l.sum().asscalar() # l 是一个 batch_size 长度的向量，每一个代表一个样本的损失
+                iter_count += l.shape[0]
             l.backward()
             trainer.step(batch_size)
-        print(f"accurate in epoch {epoch} is {cal_accuracy(test_iter)}")
+        print(f"accurate in epoch {epoch} is {cal_accuracy(test_iter)}, training loss = {total_loss/iter_count}")
 
-
+import argparse
 if __name__ == "__main__":
-        training()
+        parser = argparse.ArgumentParser(description="训练模型或者展示数据集")
+        # two subcommand : train and show
+        subparsers = parser.add_subparsers(help="sub-command help", dest="subparser_name")
+        # create the parser for the "train" command
+        parser_train = subparsers.add_parser("train", help="train help")
+        parser_train.add_argument("--epoch", type=int, default=5, help="epoch number")
+        # create the parser for the "show" command
+        parser_show = subparsers.add_parser("show", help="show help")
+        parser_show.add_argument("--num", type=int, default=18, help="number of images to show")
+        args = parser.parse_args()
+        if args.subparser_name == "train":
+            training(args.epoch)
+        elif args.subparser_name == "show":
+            X, y = mnist_train[:args.num]
+            show_images(X.squeeze(axis=-1), 2, 9, titles=get_fashion_mnist_labels(y))
+            plt.show()
     
---
+++ day2/results.md
@@ -0,0 +1,26 @@
+运行 fashion_mnist.py 的结果
+batch_size = 100, learning_rate = 0.03, epoch = 10
+(base) ➜  day2 git:(main) ✗ python fashion_mnist.py
+accurate in epoch 0 is 0.7471, training loss = 1.1199110148747762
+accurate in epoch 1 is 0.8113, training loss = 0.6273942579269409
+accurate in epoch 2 is 0.8272, training loss = 0.5383636003494263
+accurate in epoch 3 is 0.8354, training loss = 0.49776991271972654
+accurate in epoch 4 is 0.8428, training loss = 0.47342421159744263
+accurate in epoch 5 is 0.8479, training loss = 0.4558451161066691
+accurate in epoch 6 is 0.8518, training loss = 0.4418376454671224
+accurate in epoch 7 is 0.8543, training loss = 0.42991072874069214
+accurate in epoch 8 is 0.8564, training loss = 0.41936385939915977
+accurate in epoch 9 is 0.8573, training loss = 0.41003628657658897
+
+
+batch_size=200, learning_rate = 0.5, epoch = 10
+accurate in epoch 0 is 0.7946, training loss = 0.773164149093628
+accurate in epoch 1 is 0.8336, training loss = 0.4732592782974243
+accurate in epoch 2 is 0.836, training loss = 0.4134717549641927
+accurate in epoch 3 is 0.8562, training loss = 0.38267962233225505
+accurate in epoch 4 is 0.8612, training loss = 0.35901554718017575
+accurate in epoch 5 is 0.8639, training loss = 0.3411409064610799
+accurate in epoch 6 is 0.8682, training loss = 0.3283139035542806
+accurate in epoch 7 is 0.8685, training loss = 0.31482937704722086
+accurate in epoch 8 is 0.8782, training loss = 0.3040309098561605
+accurate in epoch 9 is 0.8768, training loss = 0.29466837450663247
